646976039415850e160780e9f721de0e80c648dd
Leonardo Botrel <33057752+leonardobotrel@users.noreply.github.com>
2019-04-19 09:13:14 -0300

    Feitos o nivel-5 e o nível-2

--- Codigo_Satelite.ino
+++ Codigo_Satelite.ino
@@ -1,3 +1,4 @@
+#include <Satelite.h>
 #include <SoftwareSerial.h>
  
 //SIM800 TX is connected to Arduino D8
@@ -5,9 +6,18 @@
  
 //SIM800 RX is connected to Arduino D7
 #define SIM800_RX_PIN 7
+
+String msg = "teste";
+int x = 10;
+int y = 5;
+bool escolha = false;
+float area;
+int frenquencia;
  
 //Create software serial object to communicate with SIM800
 SoftwareSerial serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN);
+
+Satelite satelite;
  
 void setup() {
   //Begin serial comunication with Arduino and Arduino IDE (Serial Monitor)
@@ -39,6 +49,64 @@ void setup() {
      
   Serial.println("SMS Sent!");
 }
+
+/*Handle serial input from the serial monitor. While there is serial information available, 
+read it into the char array. If a newline character is encountered, terminate the array 
+and return to the main program.*/
+int readSerial(char result[])
+{
+  int i = 0;
+  while(1)
+  {
+    while (Serial.available() > 0)
+    {
+      char inChar = Serial.read();
+      if (inChar == '\n')
+      {
+        result[i] = '\0';
+        return 0;
+      }
+      if(inChar!='\r')
+      {
+        result[i] = inChar;
+        i++;
+      }
+    }
+  }
+}
  
 void loop() {
+  //satelite.imprimirMensagem();
+  //escolha = satelite.escolheArea();
+
+  //Authentication process for sending messages
+  //Use the readSerial() function to get the bytes from the serial monitor
+  char login[50];
+  Serial.print("Enter your login: ");
+  readSerial(login);
+  Serial.println(login);
+
+  //Use the readSerial() function to get the bytes from the serial monitor
+  char password[20];
+  Serial.print("Enter your password: ");
+  readSerial(password);
+
+  //Verify that the password and login are correct
+  Serial.println("authenticating...");
+  if(!satelite.authenticate(login, password))
+  {
+    Serial.println("Login or Password invalid.");
+  }
+  else
+  {
+    Serial.println("authenticated.");
+  }
+  
+  
+  if (escolha == true)
+     area = satelite.areaRetangulo(x, y);
+  else
+     area = satelite.areaTriangulo(x, y);
+
+  serialSIM800.write(area);
 }
